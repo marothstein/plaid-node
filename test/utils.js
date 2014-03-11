620c69f3933ebab64f6732d54e0f630023f80eec
David Chambers <dc@davidchambers.me>
2014-03-11 16:33:20 -0700

    lint: remove unnecessary variable declarations

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,9 @@
 module.exports = {
 
   getKeys: function() {
-    var client_id = process.env.PLAID_CLIENTID,
-        secret = process.env.PLAID_SECRET;
-
     return {
-      client_id: client_id,
-      secret: secret
+      client_id: process.env.PLAID_CLIENTID,
+      secret: process.env.PLAID_SECRET
     };
   },
 
